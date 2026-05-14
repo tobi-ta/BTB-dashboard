1e3f3539452d4cc547d9b616849f9839f7af0545
tobi-ta <114591320+tobi-ta@users.noreply.github.com>
2026-05-14 13:47:20 -0400

    Update Supabase URL and key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = 'https://hrqhfbrfiqnkrtixpjxy.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_dRWEsI0-vENUz0Qqic5utg_kxrcwWsl';
+const SUPABASE_URL = 'https://iwllpnkcyixpgtpujshc.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_XbTTA346qmEF8g7runLDvg_0hGZ9rMT';
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
